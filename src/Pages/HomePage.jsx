aa98ea0cc01d31f93b8984e382d9c11cf0f7ba47
Abdul2102 <114062056+Abdul2102@users.noreply.github.com>
2025-01-30 23:56:31 +0530

    Update HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
-import Work from "../components/Work";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
